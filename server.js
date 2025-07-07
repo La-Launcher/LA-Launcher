5cc5a26d26fd510120eab34e6060166199771124
KAKXER <contact.shahsavari@gmail.com>
2025-07-07 23:54:56 +0330

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 
 app.use(express.static("website"));
 
-["", "servers", "errors", "donate"].forEach(route => {
+["index", "servers", "errors", "donate"].forEach(route => {
   app.get("/" + route, (_, res) =>
-    res.sendFile(path.join(__dirname, `website/${route || "index"}.html`))
+    res.sendFile(path.join(__dirname, `website/${route}.html`))
   );
 });
 
-app.use((_, res) => res.status(404).sendFile(path.join(__dirname, "website/404.html")));
+app.use((req, res) => res.status(404).sendFile(path.join(__dirname, "website/404.html")));
 
 app.listen(3005);
