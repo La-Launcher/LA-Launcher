eac8cd89defa2f13ec69e5852d1cb8a2c2b2a4d9
KAKXER <89189991+KAKXER@users.noreply.github.com>
2025-08-04 03:49:28 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PLATFORM_NAME = process.env.PLATFORM_NAME ?? "la5m";
+const BASE_DIR = path.join(__dirname, "website", PLATFORM_NAME);
 
-app.use(express.static("website"));
+app.use(express.static(BASE_DIR));
 
-["", "servers", "errors", "donate", "api"].forEach(route => {
-  app.get("/" + route, (_, res) => res.sendFile(path.join(__dirname, website/${PLATFORM_NAME}/${route}.html)));
+["index", "servers", "errors", "donate", "api"].forEach(route => {
+  app.get("/" + (route === "index" ? "" : route), (_, res) => res.sendFile(path.join(BASE_DIR, ${route}.html)));
 });
 
-app.use((_, res) => res.status(404).sendFile(path.join(__dirname, website/${PLATFORM_NAME}/404.html)));
+app.use((_, res) => res.status(404).sendFile(path.join(BASE_DIR, "404.html")));
 
 app.listen(3005);
