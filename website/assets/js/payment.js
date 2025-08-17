bcb8fd0b3b28ccf8e91d5b91084619b6e8dcb2e6
KAKXER <89189991+KAKXER@users.noreply.github.com>
2025-08-17 04:20:45 +0330

    upload payment page

---
+++ website/assets/js/payment.js
@@ -0,0 +1,100 @@
+import './utils/loading.dat.js';
+
+$(async function () {
+    const sendStatusToLauncher = async (status, tracking = null) => {
+        try {
+            console.log(status, tracking)
+            const result = await fetch(`http://127.0.0.1:7878/payment?status=${status}&tracking=${tracking}`, {method: "POST", headers: { "Content-Type": "application/json" }});
+
+            const text = await result.text();
+            console.log("Payment status sent:", status, tracking);
+
+            return text === "ok";
+        } catch(e) {
+            console.error("Cannot send status to launcher:", e);
+        }
+    };
+
+    const params = new URLSearchParams(window.location.search);
+    let status = params.get("status");
+    let savedTracking = localStorage.getItem("lastTrackingCode");
+
+    if (status == "null") {
+        localStorage.removeItem("lastPaymentSuccess");
+        localStorage.removeItem("lastTrackingCode");
+        status = false;
+        savedTracking = null;
+    }
+
+    const tracking = params.get("tracking");
+    const hadSuccess = localStorage.getItem("lastPaymentSuccess");
+    const renderHtml = html => $("#content-box").html(html);
+    const isSended = await sendStatusToLauncher(status || hadSuccess, tracking || savedTracking);
+
+    if (!status) {
+        if (hadSuccess === "true") {
+            document.title = "LA Platform - Already Verified";
+            renderHtml(`
+                <dotlottie-player src="../json/payment-success.lottie" speed="1" autoplay style="height:200px;opacity:0.7;margin:-20px 0;"></dotlottie-player>
+                <div>
+                <h2 class="text-2xl sm:text-3xl font-bold text-white">پرداخت شما قبلاً تایید شده است</h2>
+                <p class="text-white/60 text-md">نیازی به تایید دوباره نیست.</p>
+                </div>
+                ${savedTracking ? `<div class="bg-white/5 rounded-lg px-4 py-3 w-full max-w-xs">
+                <p class="text-white/80 text-sm mb-1">کد پیگیری:</p>
+                <p class="text-white text-lg tracking-wider select-all font-['Rajdhani_SemiBold']">${savedTracking}</p>
+                </div>
+                <p class="text-white/60 text-xs max-w-xs mt-[-10px]">لطفاً این کد را نگه دارید. در صورت بروز هرگونه مشکل یا سوال درباره سرویس، به آن نیاز خواهید داشت.</p>` : ''}
+            `);
+        } else {
+            document.title = "LA Platform - Payment Error";
+            renderHtml(`
+                <dotlottie-player src="../json/payment-failed.lottie" speed="1" autoplay style="height:200px;opacity:0.7;margin:-20px 0;"></dotlottie-player>
+                <div>
+                <h2 class="text-2xl sm:text-3xl font-bold text-white">خطا در پردازش اطلاعات 🤨</h2>
+                <p class="text-white/60 text-md mt-[10px]">
+                    متأسفانه پرداخت با خطا مواجه شد.  
+                    مبلغ پرداختی طی ۲۴ الی ۷۲ ساعت آینده به حساب شما بازگردانده می‌شود.  
+                    در صورت نیاز با تیم پشتیبانی از طریق <a href="https://discord.la5m.ir" target="_blank" class="text-[#FCD53FCC] font-bold">تیکت دیسکورد</a> در ارتباط باشید.
+                </p>
+                </div>
+            `);
+        }
+    } else if (status === "true" && tracking) {
+        document.title = "LA Platform - Payment Success";
+        localStorage.setItem("lastPaymentSuccess", "true");
+        localStorage.setItem("lastTrackingCode", tracking);
+
+        renderHtml(`
+            <dotlottie-player src="../json/payment-success.lottie" speed="1" autoplay style="height:230px;opacity:0.7;margin:-20px 0;"></dotlottie-player>
+            <div>
+                <h2 class="text-2xl sm:text-3xl font-bold text-white">سرویس شما فعال شد 🥂</h2>
+                <p class="text-sm sm:text-base text-white/70 max-w-md leading-relaxed">
+                    سرویس خریداری شده شما با موفقیت فعال شد! ${isSended ? 'اکنون می‌توانید این صفحه را ببندید و به لانچر بازگردید 🦾' : 'لانچر پاسخگو نبود! لطفاً آن را مجدد باز کنید و سرویس خود لذت ببرید 💪'}
+                </p>
+            </div>
+            
+            <div class="bg-white/5 rounded-lg px-4 py-3 w-full max-w-xs">
+                <p class="text-white/80 text-sm mb-1">کد پیگیری:</p>
+                <p class="text-white text-lg tracking-wider select-all font-['Rajdhani_SemiBold']">${tracking}</p>
+            </div>
+            <p class="text-white/60 text-xs max-w-xs mt-[-10px]">
+                لطفاً این کد را نگه دارید. در صورت بروز هرگونه مشکل یا سوال درباره سرویس، به آن نیاز خواهید داشت.
+            </p>
+        `);
+        window.history.replaceState({}, document.title, window.location.pathname);
+    } else if (status === "false") {
+        localStorage.setItem("lastPaymentSuccess", "false");
+        document.title = "LA Platform - Payment Failed";
+        renderHtml(`
+            <dotlottie-player src="../json/payment-failed.lottie" speed="1" autoplay style="height:230px;opacity:0.7;margin:-20px 0;"></dotlottie-player>
+            <div>
+                <h2 class="text-2xl sm:text-3xl font-bold text-white">پرداخت ناموفق بود 🤔</h2>
+                <p class="text-white/60 text-md mt-[10px]">
+                     پرداخت توسط شما لغو شد. لطفاً دوباره تلاش کنید و در صورت نیاز با تیم پشتیبانی از طریق <a href="https://discord.la5m.ir" target="_blank" class="text-[#FCD53FCC] font-semibold">تیکت دیسکورد</a> در ارتباط باشید.
+                </p>
+            </div>
+        `);
+        window.history.replaceState({}, document.title, window.location.pathname);
+    }
+});
