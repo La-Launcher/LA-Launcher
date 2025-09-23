8c217ab6b405f061b95b5a403a0f57a29b08e032
KAKXER <89189991+KAKXER@users.noreply.github.com>
2025-09-23 09:34:42 +0330

    Update server.info.js

--- website/assets/js/services/server.info.js
+++ website/assets/js/services/server.info.js
@@ -5,7 +5,7 @@ export function refreshServerInfo() {
     $(document).on("click", ".app-server-list-main", function(event) {
         if ($(event.target).closest('.app-server-list-server-active-boost').length) {
             const serverId = $(this).attr("id");
-            window.location.href = `https://${gameName == "gta5" ? "la5m" : "la2m"}.ir/boost?id=${serverId}`;
+            window.location.href = `https://${gameName == "gta5" ? "la5m" : "la2m"}.ir/boost?cfxid=${serverId}`;
             return;
         } else $('.download-page').removeClass('hidden opacity-0 translate-y-8').addClass('opacity-100 translate-y-0');
     });
